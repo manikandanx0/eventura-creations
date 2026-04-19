67ec76a16d2c5fe0cd189cf1c8a4806bb15933b2
Manikandan V <manikandan.v.2024.aids@rajalakshmi.edu.in>
2026-04-19 13:12:37 +0530

    reset

--- server/src/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProjectBySlug, listProjects } from '../controllers/projectController.js';
-
-const router = Router();
-
-router.get('/', listProjects);
-router.get('/:slug', getProjectBySlug);
-
-export default router;
--- server/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wraps async Express handlers so rejected promises reach `next(err)` and the error middleware.
- */
-export function asyncHandler(fn) {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
